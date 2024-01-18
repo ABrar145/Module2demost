8c5d68c79d4e348c04dab1aff878e8316f3e3e7b
Amninder Brar <abrar14@rrc.ca>
2024-01-18 13:05:48 -0600

    Initial commit.

---
+++ Module_2.py
@@ -0,0 +1,6 @@
+"""
+Description: Module 2 demostration
+Author: Amninder Brar
+Date: 18 january,2024
+Usage: to demonstrate content from module 2
+"""
---
+++ README.md
@@ -0,0 +1,7 @@
+# Module2demost
+
+## Description
+Introduction to python: Documentation, variables, printing
+
+## Author
+Amninder Brar
