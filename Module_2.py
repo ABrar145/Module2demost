b3319aabc0b4c3b1f080757ddd726fb3bf127c2e
Amninder Brar <abrar14@rrc.ca>
2024-01-18 13:38:33 -0600

    Experimented with built-in functions.

--- Module_2.py
+++ Module_2.py
@@ -4,3 +4,10 @@ Author: Amninder Brar
 Date: 18 january,2024
 Usage: to demonstrate content from module 2
 """
+
+absolute_value = abs(-12)
+print('absolute value:',abs(-12))
+
+#Functions embedded within another function.
+print('Absolute value:',abs(-12))
+print('Absolute value:',abs(-12))
